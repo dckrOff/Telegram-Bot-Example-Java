bf688b17b26747031d6a00c6417487a0fc140bde
dckrOff <azamatotaboev7621@gmail.com>
2022-08-25 12:34:42 +0500

    added inline keyboard button, but callback is not working

--- src/main/java/com/ExampleBot.java
+++ src/main/java/com/ExampleBot.java
@@ -1,12 +1,17 @@
 package com;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,19 +38,25 @@ public class ExampleBot extends TelegramLongPollingBot {
             if (message.hasText()) {
                 String text = message.getText();
                 if (text.equals("/start")) {
+                    System.out.println("start");
+
                     SendMessage sendMessage = new SendMessage();
                     sendMessage.setChatId(message.getChatId().toString());
                     sendMessage.setText("Hello world");
 
+                    // keyboard Button
                     ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
                     replyKeyboardMarkup.setResizeKeyboard(true);
+
                     List<KeyboardRow> keyboardRowList = new ArrayList<>();
+
                     KeyboardRow keyboardRow1 = new KeyboardRow();
                     KeyboardButton keyboardButton1 = new KeyboardButton();
-                    keyboardButton1.setText("Tilni tanlang");
+                    keyboardButton1.setText("Tilni tanlash");
                     keyboardRow1.add(keyboardButton1);
                     keyboardRowList.add(keyboardRow1);
                     replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
                     sendMessage.setReplyMarkup(replyKeyboardMarkup);
 
                     try {
@@ -53,7 +64,60 @@ public class ExampleBot extends TelegramLongPollingBot {
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
+                } else if (text.equals("Tilni tanlash")) {
+                    System.out.println("tilni tanlash");
+
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(message.getChatId().toString());
+                    sendMessage.setText("Tilni tanlang");
+
+                    // Inline keyboard button
+                    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                    List<List<InlineKeyboardButton>> inlineButtons = new ArrayList<>();
+                    List<InlineKeyboardButton> inlineKeyboardButtonList = new ArrayList<>();
+
+                    InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+
+                    inlineKeyboardButton1.setText("Русский язык");
+                    inlineKeyboardButton2.setText("O'zbek tili");
+
+                    inlineKeyboardButton1.setCallbackData("russian");
+                    inlineKeyboardButton2.setCallbackData("uzbek");
+
+                    inlineKeyboardButtonList.add(inlineKeyboardButton1);
+                    inlineKeyboardButtonList.add(inlineKeyboardButton2);
+
+                    inlineButtons.add(inlineKeyboardButtonList);
+
+                    inlineKeyboardMarkup.setKeyboard(inlineButtons);
+                    sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+
+                    try {
+                        execute(sendMessage);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+            }
+        } else if (update.hasCallbackQuery()) {
+            Message message = update.getMessage();
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(message.getChatId().toString());
+
+            if (data.equals("russian")) {
+                sendMessage.setText("Выбран русский язык");
+            } else if (data.equals("uzbek")) {
+                sendMessage.setText("O'zbek tili tanlandi");
+            } else {
+            }
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
     }
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,7 +4,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import javax.annotation.PostConstruct;
+
 public class Main {
+    @PostConstruct
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
