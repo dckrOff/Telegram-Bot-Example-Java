c9a77dba4227448d0268c8eb9163d28f85fa6410
dckrOff <azamatotaboev7621@gmail.com>
2022-08-25 11:27:14 +0500

    added keyboardButton

--- src/main/java/com/ExampleBot.java
+++ src/main/java/com/ExampleBot.java
@@ -4,26 +4,17 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExampleBot extends TelegramLongPollingBot {
 
     private String BOT_USERNAME = "weatherlivepybot";
     private String BOT_TOKEN = "761235389:AAFlBgDe8a9yZFU7wpwtjytJHVqY63PGLXQ";
-
-    private String START_MESSAGE = "Привет!\n" +
-            "Это мой первый бот написанный на языке JAVA. \n" +
-            "Пока что доступны только некоторые команда: \n" +
-            "/help - список всех команд \n" +
-            "/calc - калькулятор \n" +
-            "/start - запуск бота \n" +
-            "/stop - остановка бота \n\n" +
-            "Скоро доработаю этого бота!";
-
-    private String HELP_MESSAGE = "Список всех доступных команд: \n" +
-            "/help - список всех команд \n" +
-            "/calc - калькулятор \n" +
-            "/start - запуск бота \n" +
-            "/stop - остановка бота";
 
     @Override
     public String getBotUsername() {
@@ -37,41 +28,33 @@ public class ExampleBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        try {
-            if (update.hasMessage()) {
-                Message message = update.getMessage();
-                if (message.hasText()) {
-                    String text = message.getText();
-                    if (text.equals("/start")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setText(START_MESSAGE);
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+                if (text.equals("/start")) {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(message.getChatId().toString());
+                    sendMessage.setText("Hello world");
+
+                    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+                    replyKeyboardMarkup.setResizeKeyboard(true);
+                    List<KeyboardRow> keyboardRowList = new ArrayList<>();
+                    KeyboardRow keyboardRow1 = new KeyboardRow();
+                    KeyboardButton keyboardButton1 = new KeyboardButton();
+                    keyboardButton1.setText("Tilni tanlang");
+                    keyboardRow1.add(keyboardButton1);
+                    keyboardRowList.add(keyboardRow1);
+                    replyKeyboardMarkup.setKeyboard(keyboardRowList);
+                    sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+                    try {
                         execute(sendMessage);
-                    } else if (text.contains("Ты кто")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setText("Я тот кто тебя захуярит)");
-                        execute(sendMessage);
-                    } else if (text.contains("/calc")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setText("https://okcalc.com/ru/");
-                        execute(sendMessage);
-                    } else if (text.contains("/help")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setText(HELP_MESSAGE);
-                        execute(sendMessage);
-                    } else if (text.contains("/stop")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setText("Bot was stopped!");
-                        execute(sendMessage);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
